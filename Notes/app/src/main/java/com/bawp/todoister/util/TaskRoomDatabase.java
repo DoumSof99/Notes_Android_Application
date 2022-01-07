01c422ad26bc3b8eae00e3a16c83bc48abebbf35
DoumSof99 <sofiadmn99@gmail.com>
2022-01-07 16:52:42 +0200

    add class DaoTask, Dao

---
+++ Notes/app/src/main/java/com/bawp/todoister/util/TaskRoomDatabase.java
@@ -0,0 +1,53 @@
+package com.bawp.todoister.util;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import com.bawp.todoister.data.TaskDao;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public abstract class TaskRoomDatabase extends RoomDatabase {
+
+    public static final int NUMBER_OF_TREATS = 4;
+    public static final String DATASE_NAME = "todoister_databse";
+    public static volatile TaskRoomDatabase INSTANCE;
+    public static final ExecutorService databaseWriterExecutor
+            = Executors.newFixedThreadPool(NUMBER_OF_TREATS);
+
+    public static final RoomDatabase.Callback sRoomDatabaseCallback =
+            new RoomDatabase.Callback() {
+                @Override
+                public void onCreate(@NonNull SupportSQLiteDatabase db) {
+                    super.onCreate(db);
+                    databaseWriterExecutor.execute(() -> {
+                        TaskDao taskDao = INSTANCE.taskDao();
+                        taskDao.deleteAll();
+                        // invoke Dao, and Write
+                    });
+                }
+            };
+
+    public static TaskRoomDatabase getDatabase(final Context context) {
+        if (INSTANCE == null) {
+            synchronized ((TaskRoomDatabase.class)) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            TaskRoomDatabase.class, DATASE_NAME)
+                            .addCallback(sRoomDatabaseCallback)
+                            .build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
+
+    public abstract TaskDao taskDao();
+
+
+}
